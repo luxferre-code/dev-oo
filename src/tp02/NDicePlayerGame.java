94ccda7f6cc415130b666e980f28c38b163ab847
LuxFerre <valentin.thuillier@luxferre-code.fr>
2023-02-01 19:21:27 +0100

    finish

---
+++ src/tp02/NDicePlayerGame.java
@@ -0,0 +1,74 @@
+package tp02;
+
+import tpOO.util.Keyboard;
+
+public class NDicePlayerGame {
+
+    private DicePlayer[] tabPlayer;
+    private final int MAX_TOTAL_VALUE = 50;
+    private final Dice DE = new Dice(6);
+    
+    NDicePlayerGame(int nbJoeur) {
+        this.tabPlayer = new DicePlayer[nbJoeur];
+        for(int i = 0; i < this.tabPlayer.length; i++) this.tabPlayer[i] = new DicePlayer("Player" + (i + 1));
+    }
+
+    public void playGame() {
+        /*
+         * Permet de faire jouer tous les joueurs
+         */
+        for(DicePlayer player : this.tabPlayer) {
+            while(player.getTotalValue() < MAX_TOTAL_VALUE) player.play(this.DE);
+        }
+    }
+
+    private int dernierVeritableElement(DicePlayer[] tab) {
+        /*
+         * Permet de trouver le dernier élément d'un tableau
+         */
+        int i = 0;
+        while(i < tab.length && tab[i] != null) i++;
+        return i;
+    }
+
+    private DicePlayer[] addElementInTab(DicePlayer[] tab, DicePlayer player) {
+        /* 
+         * Permet d'ajouter à un tableau défini à un taille fixe un joueur
+        */
+        int rang = dernierVeritableElement(tab);
+        if(rang < tab.length) tab[rang] = player;
+        else {
+            DicePlayer[] newTab = new DicePlayer[tab.length * 2];
+            for(int i = 0; i < tab.length; i++) newTab[i] = tab[i];
+            newTab[tab.length] = player;
+            tab = newTab;
+        }
+        return tab;
+    }
+
+    public DicePlayer[] winner() {
+        /*
+         * Permet de trouver le ou les gagnants
+         */
+        DicePlayer[] winner = new DicePlayer[]{this.tabPlayer[0]};
+        for(int i = 1; i < this.tabPlayer.length; i++) {
+            if(this.tabPlayer[i].getTotalValue() > winner[0].getTotalValue()) {
+                winner = new DicePlayer[]{this.tabPlayer[i]};
+            } else if(this.tabPlayer[i].getTotalValue() == winner[0].getTotalValue()) {
+                winner = addElementInTab(winner, this.tabPlayer[i]);
+            }
+        }
+        return winner;        
+    }
+
+    public static void main(String[] args) {
+        NDicePlayerGame game = new NDicePlayerGame(util.Keyboard.readInt("Nombre de joueurs: "));
+        game.playGame();
+        System.out.println("Gagnant(s):");
+        for(DicePlayer player : game.winner()) {
+            if(player != null) System.out.println(player);
+        }
+
+    }
+
+}
